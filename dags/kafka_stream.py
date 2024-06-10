9c97b3f817e2bd4204d39eb5d113f7a802182531
Jay Joshi <jayjoshi112711@gmail.com>
2024-06-10 06:57:09 -0700

    Added functionalities to stream data

---
+++ dags/kafka_stream.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airscholar',
+    'start_date': datetime(2024, 5, 9, 10, 00)
+}
+
+def get_data():
+    import json
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+    # print(json.dumps(res, indent=4))
+    return res # returns a dictionary
+    # Code to stream data from API
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {str(location['street']['name'])}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+def stream_data():
+    import json
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=4))
+
+
+with DAG('user_automation', 
+         default_args=default_args,
+         schedule='@daily',
+         catchup=False) as dag:
+    
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+        )
+
+stream_data()
